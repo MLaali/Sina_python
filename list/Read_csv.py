ae05b44563ebc31f1688fb4ef5bb2f479182196d
MLaali <39208885+MLaali@users.noreply.github.com>
2021-05-01 02:00:04 +0430

    Update Read_csv.py

--- list/Read_csv.py
+++ list/Read_csv.py
@@ -1,12 +1,12 @@
 import csv
 
 def read_csv(file_name,file_data):
-    file_data=[]
+    file_data.clear()
     file= open(file_name, mode='r')
     reader = csv.reader(file)
     for row in reader:
         file_data.append(row)
 
-
+poeple=[]
 read_csv('people.csv',poeple)
-print(people)
+print(len(poeple))
